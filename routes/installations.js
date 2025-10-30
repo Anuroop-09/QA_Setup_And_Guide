bdd7f8124ae1a7f78b1623131e03aa757f3835cb
Anuroop Pamarthi <Anuroop@DESKTOP-JLDIC5L>
2025-10-30 07:35:01 +0530

    Updated Routes

--- routes/installations.js
+++ routes/installations.js
@@ -1,10 +1,16 @@
 import express from "express";
 const router = express.Router();
 
-import { renderJavaInstallationFrame } from "../controllers/installationsController.js";
-import { renderMavenInstallationFrame } from "../controllers/installationsController.js";
+import { 
+    renderJavaInstallationFrame,
+    renderMavenInstallationFrame,
+    renderEclipseInstallationFrame,
+    renderJenkinsInstallationFrame
+} from "../controllers/installationsController.js";
 
 router.get("/java_installation", renderJavaInstallationFrame);
 router.get("/maven_installation", renderMavenInstallationFrame);
+router.get("/eclipse_installation", renderEclipseInstallationFrame);
+router.get("/jenkins_installation", renderJenkinsInstallationFrame);
 
 export default router;
